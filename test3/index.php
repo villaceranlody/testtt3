66ca4a0f0231180129c1e8e2a91afd82cf17976b
villaceranlody <146922763+villaceranlody@users.noreply.github.com>
2024-11-07 15:14:26 +0800

    Add files via upload

---
+++ test3/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>My first Ubuntu Server PHP Deployment</title>
+</head>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<body>
+     	<p>My first Ubuntu Server PHP Deployment</p>
+</body>
+	<div>
+		<label>Firstname:</label>
+		<input type="text" name="Firstname" class="form-control">
+	</div>
+
+	<div>
+		<label>Middle Name:</label>
+		<input type="text" name="middlename" class="form-control">
+	</div>
+
+    <div>
+		<label>Lastname:</label>
+		<input type="text" name="lastname" class="form-control">
+	</div>
+</html>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
